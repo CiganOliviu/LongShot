450e7bbad3f7c10a59a9da43adebfa990b5bfeb3
Cigan Oliviu David <ciganoliviudavid@gmail.com>
2020-11-29 00:26:16 +0200

    Update LongShot

---
+++ LongShot/app/Http/Controllers/TourAdministrationController.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Tour;
+use Illuminate\Http\Request;
+
+class TourAdministrationController extends Controller
+{
+
+    public function TourAdministrationView()
+    {
+        return view('administration/tour_administration');
+    }
+
+    public function SaveDataToTourTableViews(Request $request)
+    {
+        $TourQuerySave = Tour::updateOrCreate(
+            [   'location' => $request->location, 'space' => $request->space,
+                'ticket' => $request->ticket, 'time' => $request->time
+            ],
+            [   'location' => $request->location, 'space' => $request->space,
+                'ticket' => $request->ticket, 'time' => $request->time
+            ],
+        );
+
+        $TourQuerySave->save();
+
+        return view('administration/tour_administration_success');
+    }
+}
